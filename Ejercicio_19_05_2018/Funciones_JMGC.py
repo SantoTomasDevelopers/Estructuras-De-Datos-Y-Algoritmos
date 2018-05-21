dccb17d9cb0a78ece6ebdfd32a90428d1a18f050
zh4r0nax <30630616+zh4r0nax@users.noreply.github.com>
2018-05-20 23:00:22 -0400

    Calculo Nota baja

--- Ejercicio_19_05_2018/Funciones_JMGC.py
+++ Ejercicio_19_05_2018/Funciones_JMGC.py
@@ -7,23 +7,30 @@
 #jprodrigues@rodriges.cl
 
 
-PonderacionTeorico = 20
+PonderacionTeorico  = 20
 ponderacionPractico = 12
 PonderacionProyecto = 48
-PonderacionExamen = 20
-NombreEstudiante = ''
+PonderacionExamen   = 20
+NombreEstudiante    = ''
+NotaBaja            = 0
 
 def IngreseNombre():
 	return input('Ingrese su nombre ::: ')
 
 def TomaNotas(CantidadNotas,TipoNota):
+	global NotaBaja
 	if(CantidadNotas != 0):
 		Suma = 0
 		Promedio = 0
 		for i in range(CantidadNotas):
 			Nota = float(input('Ingrese '+TipoNota+' N° : '+str(i+1)+' :: '))
+			if(NotaBaja == 0):
+				NotaBaja = Nota
+			if(Nota<NotaBaja):
+				NotaBaja = Nota
 			Suma = Nota + Suma 
 			#print(Suma)
+		Suma -= NotaBaja
 
 		print(Suma)
 		print(CantidadNotas)
