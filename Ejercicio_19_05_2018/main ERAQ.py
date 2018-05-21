0f5fc995b6f4f417bf5747954fd5b619f15142b9
XxRipredXx <39245282+XxRipredXx@users.noreply.github.com>
2018-05-21 02:38:27 -0300

    Update main ERAQ.py

--- Ejercicio_19_05_2018/main ERAQ.py
+++ Ejercicio_19_05_2018/main ERAQ.py
@@ -1,19 +1,23 @@
 #main
 
-import funciones
-nombre = funciones.nombre()
-print"Bienvenido",nombre
+import funciones_ERAQ
+entrada=1
+while (entrada !=0):
+    nombre = funciones_ERAQ.nombre()
+    print "Bienvenido",nombre
 
-cantidad_de_notas=int(input("ingrese cantidad de pruebas: "))
-controles_teoricos = funciones.nota_controles_teoricos(cantidad_de_notas)
-print "el promedio de controles teoricos es: ", controles_teoricos
-controles_practicos= funciones.nota_controles_practicos(cantidad_de_notas)
-print "Su promedio de controles practicos es: ", controles_practicos
-nota_de_proyecto = funciones.nota_proyecto()
-print "Nota del proyecto: ",nota_de_proyecto
+    cantidad_de_notas=int(input("ingrese cantidad de pruebas: "))
+    controles_teoricos = funciones_ERAQ.nota_controles_teoricos(cantidad_de_notas)
+    print "el promedio de controles teoricos es: ", controles_teoricos
+    controles_practicos= funciones_ERAQ.nota_controles_practicos(cantidad_de_notas)
+    print "Su promedio de controles practicos es: ", controles_practicos
+    nota_de_proyecto = funciones_ERAQ.nota_proyecto()
+    print "Nota del proyecto: ",nota_de_proyecto
 
-sumaPromedios=(controles_teoricos+controles_practicos+nota_de_proyecto)
-promedioTotal=sumaPromedios / 3
-print ("Su Nota de presentacion es: ",promedioTotal)
+    sumaPromedios=(controles_teoricos+controles_practicos+nota_de_proyecto)
+    promedioTotal=sumaPromedios / 3
+    print "Su Nota de presentacion es: ",promedioTotal
 
-presentacion = funciones.eximido_o_reprobado(promedioTotal)
+    presentacion = funciones_ERAQ.eximido_o_reprobado(promedioTotal)
+
+    entrada=int(input("Desea ingresar otro alumno (presione 0 para salir)"))
