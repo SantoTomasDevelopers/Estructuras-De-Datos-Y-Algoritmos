2beabef9bf907cc8dd84298b8c03f7071a9e33ca
zh4r0nax <30630616+zh4r0nax@users.noreply.github.com>
2018-05-21 01:49:40 -0400

    casi

--- Ejercicio_19_05_2018/Funciones_JMGC.py
+++ Ejercicio_19_05_2018/Funciones_JMGC.py
@@ -14,58 +14,92 @@ NombreEstudiante    = ''
 NotaBaja            = 0
 
 def IngreseNombre():
-	return input('Ingrese su nombre ::: ')
+	# Nombre =  input('Ingrese su Nombre ::: ')
+	# Apellido =  input('Ingrese su Apellido ::: ')
+	# return Nombre + ' ' + Apellido
+	return input('Ingrese su Nombre ::: ')
 
 def TomaNotas(CantidadNotas,TipoNota):
 	#VarPonderaciones = __init__()
 	#NotaBaja = VarPonderaciones.NotaBaja
 	global NotaBaja
-	Nota = 0
+	NotaBaja = 0.0
+	Nota = 0.0
 	if(CantidadNotas != 0):
-		Suma = 0
-		Promedio = 0
+		Suma = 0.0
+		Promedio = 0.0
 		for i in range(CantidadNotas):
-			Nota = 0
+			Nota = 0.0
 			while(Nota<=0 or Nota >7.0):
-				Nota = float(input('Ingrese '+TipoNota+' N° : '+str(i+1)+' :: '))
+				try:
+					Nota = float(input('Ingrese '+TipoNota+' N° : '+str(i+1)+' :: '))
+				except Exception as e:
+					error = format(e)
+
 			if(NotaBaja == 0):
 				NotaBaja = Nota
 			if(Nota<NotaBaja):
 				NotaBaja = Nota
 			Suma += Nota 
-			#print(Suma)
 		Suma -= NotaBaja
-
-		print(Suma)
-		print(CantidadNotas)
-		Promedio = Suma	/ CantidadNotas
-		print(Promedio)
+		Promedio = Suma	/ (CantidadNotas-1)
 		if(TipoNota == 'Control'):
-			return (PonderacionTeorico * Promedio)/100
+			return (PonderacionTeorico * Promedio)/100 , Promedio
 
 		elif(TipoNota == 'Practico'):
-			return (ponderacionPractico * Promedio)/100
+			return (ponderacionPractico * Promedio)/100 , Promedio
 
 		elif(TipoNota == 'Proyecto'):
-			return (PonderacionProyecto* Promedio)/100
+			return (PonderacionProyecto* Promedio)/100 , Promedio
 
 		elif(TipoNota == 'Examen'):
-			return (PonderacionExamen * Promedio)/100
+			return (PonderacionExamen * Promedio)/100 , Promedio
 
 	else:
 		return 0
 
 
-def ValidacionNotas(NotaControl,NotaPractico):
+def ValidacionNotas(NotaControl,NotaPractico,NotaProyecto):
 	Suma = 0
-	if(NotaControl < 4.0 or NotaPractico < 4.0):
-		#reprobado
-		return 0
-	elif( NotaControl + NotaPractico >= 5.5):
-		#aprobado
-		return 1
+	MensajeAprobado       = 'Usted Esta Aprobado y EXIMIDO del Examen!'
+	MensajeRendir         = 'Usted debe RENDIR el Examen!'
+	MensajeReprobado      = 'Usted Esta Reprobado! Por tener'
+	MensajeReprobadoDos   = 'promedio ROJO!'
+	MensajeReprobadoFinal = ''
+	MesnsajeError         = 'Error'
+	promedio 		      = 0
+	Rojos				  = ''
 
-	return 0
+	Suma =NotaControl +  NotaPractico + NotaProyecto
+	promedio = Suma / 3
+	if(NotaControl < 4.0 or NotaPractico < 4.0 or NotaProyecto < 4.0):
+		#Reprobado
+		#print(MensajeReprobado)
+		if(NotaControl < 4.0 and NotaPractico >= 4.0 and NotaProyecto >= 4.0):#1 promedio rojo
+			Rojos = 'Control : '+str(NotaControl)
+		elif(NotaControl < 4.0 and NotaPractico < 4.0 and NotaProyecto >= 4.0):#2 promedios rojos
+			Rojos = 'Control : '+str(NotaControl) +' y Practico : ' + str(NotaPractico)
+		elif(NotaControl < 4.0 and NotaPractico < 4.0 and NotaProyecto < 4.0):#3Promedios rojos
+			Rojos = 'Control : '+str(NotaControl) +' y Practico : ' + str(NotaPractico) +' y Proyecto : ' + str(NotaProyecto)
+		else:
+			Rojos = 'Error 02'
+		MensajeReprobadoFinal = MensajeReprobado+' '+Rojos+' '+MensajeReprobadoDos
+		return 0,MensajeReprobadoFinal,promedio
 
-print(ValidacionNotas(5.6,5.6))
+	elif( promedio >= 5.5):
+		#Aprobado
+		#print(MensajeAprobado)	
+		
+		return 1,MensajeAprobado,promedio
+	
+	elif( promedio >= 4.0):
+		#Rendir Examen
+		#print(MensajeRendir)
+		Suma =NotaControl +  NotaPractico + NotaProyecto
+		
+		return 2,MensajeRendir,promedio
+
+	return 0,MesnsajeError
+
+#ValidacionNotas(5.6,5.6)
 
--- Ejercicio_19_05_2018/main_JMGC.py
+++ Ejercicio_19_05_2018/main_JMGC.py
@@ -1,28 +1,40 @@
 import Funciones_JMGC as fn
-
-PonderacionControles = 0
-PonderacionPractico  = 0
-PonderacionProyecto  = 0
-PonderacionExamen    = 0
-PonderacionSuma      = 0
-Nombre = fn.IngreseNombre()
-
-PonderacionControles = fn.TomaNotas(5,'Control')
-PonderacionPractico  = fn.TomaNotas(5,'Practico')
-
-ResultadoValidacion = fn.ValidacionNotas(PonderacionControles,PonderacionPractico)
+Opcion = 'si'
 
 
+while(Opcion.upper() == 'SI' ):
+    PonderacionControles = 0
+    PonderacionPractico  = 0
+    PonderacionProyecto  = 0
+    PonderacionExamen    = 0
+    PonderacionSuma      = 0
+    Nombre               = ''
+    NotaAprobacion       = 0.0
+
+    Nombre = fn.IngreseNombre()
+    PonderacionControles = fn.TomaNotas(5,'Control')
+    PonderacionPractico  = fn.TomaNotas(5,'Practico')
+    PonderacionProyecto  = fn.TomaNotas(5,'Proyecto')
+
+    ResultadoValidacion = fn.ValidacionNotas(PonderacionControles[1],PonderacionPractico[1],PonderacionProyecto[1])
+
+    if(ResultadoValidacion[0] == 1):#Aprobado sin rendir examen
+        NotaAprobacion = ResultadoValidacion[2]
+        print('Alumno:: '+str(Nombre)+', '+str(ResultadoValidacion[1])+' con el promedio :: '+str(NotaAprobacion))
+        
+    if(ResultadoValidacion[0] == 0):#Reprobado sin rendir examen
+        print(ResultadoValidacion[1])
+        
+    if(ResultadoValidacion[0] == 2):#Rendir examen
+        print('Alumno:: '+str(Nombre)+', '+ResultadoValidacion[1])
 
 
-# PonderacionProyecto  = fn.TomaNotas(5,'Proyecto')
-# PonderacionExamen    = fn.TomaNotas(5,'Examen')
+    while(Opcion.upper() != 'NO'):
+        try:
+            Opcion = input('Decea ingresar otro alumno? \n\t\t si/no :: ')
+            if(Opcion.upper()=='SI'):
+                break
+        except Exception as e:
+            error = format(e)
 
-PonderacionSuma = PonderacionControles + PonderacionPractico + PonderacionProyecto + PonderacionExamen
-
-print(Nombre)
-print('Ponderacion Contoles :: '+str(PonderacionControles))
-print('Ponderacion Practico :: '+str(PonderacionPractico))
-print('Ponderacion Proyecto :: '+str(PonderacionProyecto))
-print('Ponderacion Examen   :: '+str(PonderacionExamen))
-print('Ponderacion Suma   :: '+str(PonderacionSuma))
+print('**************SALIENDO**************')
